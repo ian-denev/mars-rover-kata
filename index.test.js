d6699fbd48810136149edd57abd18803f120f1c0
Ian Denev <contact@iandenev.com>
2021-10-26 15:28:30 +0100

    test: Added createPlateau test

--- index.test.js
+++ index.test.js
@@ -1,1 +1,15 @@
-const { calculateRoverPosition } = require("./index");
+const { calculateRoverPosition, createPlateau } = require("./index");
+
+describe("Combined testing suite", () => {
+    test("createPlateau Test", () => {
+        const plateauSize = [6, 5];
+        const plateau = [
+            [undefined, undefined, undefined, undefined, undefined, undefined],
+            [undefined, undefined, undefined, undefined, undefined, undefined],
+            [undefined, undefined, undefined, undefined, undefined, undefined],
+            [undefined, undefined, undefined, undefined, undefined, undefined],
+            [undefined, undefined, undefined, undefined, undefined, undefined],
+        ]
+        expect(createPlateau(plateauSize)).toEqual(plateau);
+    })
+})
