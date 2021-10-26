5a52be14ca9b0ecc72241ba3ffacc8b1f4a2ad5d
Ian Denev <contact@iandenev.com>
2021-10-26 17:01:07 +0100

    test: createPlateau 6x5, 2x7, 1x1, 0x1, 1x0

--- index.test.js
+++ index.test.js
@@ -1,15 +1,43 @@
 const { calculateRoverPosition, createPlateau } = require("./index");
 
-describe("Combined testing suite", () => {
-    test("createPlateau Test", () => {
+describe("createPlateau testing suite", () => {
+    test("createPlateau 6x5 Test", () => {
         const plateauUpperRightCoordinates = [6, 5];
         const plateau = [
             [undefined, undefined, undefined, undefined, undefined, undefined],
             [undefined, undefined, undefined, undefined, undefined, undefined],
             [undefined, undefined, undefined, undefined, undefined, undefined],
             [undefined, undefined, undefined, undefined, undefined, undefined],
-            [undefined, undefined, undefined, undefined, undefined, undefined],
+            [undefined, undefined, undefined, undefined, undefined, undefined]
         ]
         expect(createPlateau(plateauUpperRightCoordinates)).toEqual(plateau);
+    })
+    test("createPlateau 2x7 Test", () => {
+        const plateauUpperRightCoordinates = [2, 7];
+        const plateau = [
+            [undefined, undefined],
+            [undefined, undefined],
+            [undefined, undefined],
+            [undefined, undefined],
+            [undefined, undefined],
+            [undefined, undefined],
+            [undefined, undefined]
+        ]
+        expect(createPlateau(plateauUpperRightCoordinates)).toEqual(plateau);
+    })
+    test("createPlateau 1x1 Test", () => {
+        const plateauUpperRightCoordinates = [1, 1];
+        const plateau = [
+            [undefined]
+        ]
+        expect(createPlateau(plateauUpperRightCoordinates)).toEqual(plateau);
+    })
+    test("createPlateau 0x1 Test", () => {
+        const plateauUpperRightCoordinates = [0, 1];
+        expect(() => createPlateau(plateauUpperRightCoordinates)).toThrow("x cannot be 0");
+    })
+    test("createPlateau 1x0 Test", () => {
+        const plateauUpperRightCoordinates = [1, 0];
+        expect(() => createPlateau(plateauUpperRightCoordinates)).toThrow("y cannot be 0");
     })
 })
