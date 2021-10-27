38f23a078f1c8d3349280b787c50305f24eff829
Ian Denev <contact@iandenev.com>
2021-10-27 05:27:31 +0100

    refactor: added comments

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const calculateRoverPosition = (platCoord, [roverX, roverY], roverOri, roverMovement, previousRoverPositions) => {
+    // Initial out-of-bounds and obstruction checks
     previousRoverPositions?.forEach(prevPos => {
         if (roverX == prevPos[0][0] && roverY == prevPos[0][1]) throw new Error("Another rover is already at this position");
     })
     if (roverX < 0 || roverX > platCoord[0]) throw new Error("Initial placement on X axis not allowed");
     else if (roverY < 0 || roverY > platCoord[1]) throw new Error("Initial placement on Y axis not allowed");
+
     [...roverMovement].forEach((letter, index) => {
         switch (letter) {
+            // Rotate position
             case "L": roverOri == "N" ? roverOri = "W" : roverOri == "W" ? roverOri = "S" :
                 roverOri == "S" ? roverOri = "E" : roverOri == "E" ? roverOri = "N" : ""; break;
             case "R": roverOri == "N" ? roverOri = "E" : roverOri == "E" ? roverOri = "S" :
                 roverOri == "S" ? roverOri = "W" : roverOri == "W" ? roverOri = "N" : ""; break;
+
+            // Movement
             case "M":
                 switch (roverOri) {
                     case "N": roverY++; break;
@@ -17,6 +22,8 @@ const calculateRoverPosition = (platCoord, [roverX, roverY], roverOri, roverMove
                     case "S": roverY--; break;
                     case "W": roverX--; break;
                 }
+
+                // Out-of-bounds and obstruction checks for every movement
                 if (roverX < 0 || roverX > platCoord[0]) throw new Error(`Movement on X axis at index ${index} not allowed`);
                 else if (roverY < 0 || roverY > platCoord[1]) throw new Error(`Movement on Y axis at index ${index} not allowed`);
                 previousRoverPositions?.forEach(prevPos => {
